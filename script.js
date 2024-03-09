8399043a7d0f981bd1c35a5950ee1c551f6539c8
ali <bovaqulov@gmail.com>
2024-03-09 16:56:48 +0500

    us

---
+++ script.js
@@ -0,0 +1,22 @@
+var yosh= +prompt("Sizning yoshingiz")
+if (yosh > 18) alert("siz ishlashingiz mumkin")
+else alert(
+    "siz hali yoshsiz oqing"
+)
+    console.log("sizning yoshingiz "+yosh);
+    if (yosh > 50) alert("Yaqinda pensiyaga chiqasiz")
+   
+    var yosh= +prompt("Sizning yoshingiz")
+    if (yosh > 59) alert("Yaqinda pensiyaga chiqasiz")
+else alert(
+    "ishlashingiz kerak"
+)
+    console.log("sizning yoshingiz "+yosh);
+    
+    var yosh= +prompt("Sizning yoshingiz")
+    if (yosh > 150) alert("Siz pensiyaga alaqachon chiqansiz trik bosez")
+else alert(
+    "siz yaqinda pensiyaga chiqasiz"
+)
+    console.log("sizning yoshingiz "+yosh);
+    
